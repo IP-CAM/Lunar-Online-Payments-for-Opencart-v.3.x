0496300f54a865f2c18f9fc94fcb9c5d2f4e4d7c
lucionescu <lucian.ionesq@gmail.com>
2023-10-09 11:34:12 +0300

    Removed unused method

--- lunar.ocmod/upload/catalog/model/extension/payment/lunar_transaction.php
+++ lunar.ocmod/upload/catalog/model/extension/payment/lunar_transaction.php
@@ -4,65 +4,6 @@ class ModelExtensionPaymentLunarTransaction extends Model
 {
     const LUNAR_DB_TABLE = DB_PREFIX . 'lunar_transaction';
 
-    public function getTransactions($data = array())
-    {
-        $sql = "SELECT * FROM `" . self::LUNAR_DB_TABLE . "` WHERE history = '0'";
-
-        if (!empty($data['filter_order_id'])) {
-            $sql .= " AND order_id = '" . (int)$data['filter_order_id'] . "'";
-        } else {
-            $sql .= " AND order_id > 0";
-        }
-
-        if (!empty($data['filter_transaction_id'])) {
-            $sql .= " AND transaction_id = '" . $data['transaction_id'] . "'";
-        }
-
-        if (!empty($data['filter_transaction_type'])) {
-            $sql .= " AND transaction_type = '" . $data['filter_transaction_type'] . "'";
-        }
-
-        if (!empty($data['filter_date_added'])) {
-            $sql .= " AND DATE(date_added) = DATE('" . $this->db->escape($data['filter_date_added']) . "')";
-        }
-
-        $sort_data = array(
-            'order_id',
-            'transaction_id',
-            'transaction_type',
-            'date_added'
-        );
-
-        if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
-            $sql .= " ORDER BY " . $data['sort'];
-        } else {
-            $sql .= " ORDER BY order_id";
-        }
-
-        if (isset($data['order']) && ($data['order'] == 'ASC')) {
-            $sql .= " ASC";
-        } else {
-            $sql .= " DESC";
-        }
-
-        if (isset($data['start']) || isset($data['limit'])) {
-            if ($data['start'] < 0) {
-                $data['start'] = 0;
-            }
-
-            if ($data['limit'] < 1) {
-                $data['limit'] = 20;
-            }
-
-            $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
-        }
-
-        $query = $this->db->query($sql);
-
-        return $query->rows;
-    }
-
-
     /**
      * GET last transaction by query data parameters
      */
