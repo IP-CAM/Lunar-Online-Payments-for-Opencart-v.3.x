fea5d8d05be21f927fbb14916f3d6a3489b60e3f
lucionescu <lucian.ionesq@gmail.com>
2023-10-16 16:10:16 +0300

    Added missing helper file

---
+++ lunar.ocmod/upload/system/library/Lunar/helper/LunarHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class LunarHelper
+{
+    const LUNAR_DB_TABLE = DB_PREFIX . 'lunar_transaction';
+
+    const LUNAR_GENERAL_PATH = 'extension/payment/lunar';
+
+    const LUNAR_METHODS = [
+        'card' => 'lunar_card',
+        'mobilePay' => 'lunar_mobilepay',
+    ];
+
+    const LUNAR_CARD_CODE = 'card';
+    const LUNAR_CARD_CONFIG_CODE = 'payment_' . self::LUNAR_METHODS[self::LUNAR_CARD_CODE];
+
+    const LUNAR_MOBILEPAY_CODE = 'mobilePay';
+    const LUNAR_MOBILEPAY_CONFIG_CODE = 'payment_' . self::LUNAR_METHODS[self::LUNAR_MOBILEPAY_CODE];
+
+    public static function pluginVersion()
+    {
+        return json_decode(file_get_contents(dirname(__DIR__) . '/composer.json'))->version;
+    }
+}
